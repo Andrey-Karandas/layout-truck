dbd39e6f362bdfd83b0252d13e1ebd023739b5c8
Andriy <jasperheroqt17@gmail.com>
2024-09-07 13:13:48 +0300

    update-function

--- js/acord.js
+++ js/acord.js
@@ -1,21 +1,7 @@
-const setClass = (name) =>
-  document.querySelector(name).classList.toggle('active')
-
-const getParent = (name) => event.target.closest(name)
-
-const arrow1 = '.column__arrow_1'
-const arrow2 = '.column__arrow_2'
-const arrow3 = '.column__arrow_3'
-
 document.addEventListener('click', (event) => {
-  if (getParent(arrow1)) {
-    setClass('.column__list_1')
-    setClass(arrow1)
-  } else if (getParent(arrow2)) {
-    setClass('.column__list_2')
-    setClass(arrow2)
-  } else if (getParent(arrow3)) {
-    setClass('.column__list_3')
-    setClass(arrow3)
+  const arrow = event.target.closest('.column__arrow')
+  if (arrow) {
+    const id = arrow.getAttribute('data-id')
+    document.querySelector(`[data-id="${id}"`).classList.toggle('active')
   }
 })
--- js/burger.js
+++ js/burger.js
@@ -1,16 +1,10 @@
 const body = document.body
-const obj = {
-  menu: document.querySelector('.header__menu'),
-  burger: document.querySelector('.header__burger'),
-}
+const headerBody = document.querySelector('.header__body')
 
 document.addEventListener('click', (event) => {
   event.preventDefault()
   if (event.target.closest('.header__burger')) {
     body.classList.toggle('lock')
-    Object.keys(obj).forEach((item) => obj[item].classList.toggle('active'))
-  } else if (event.target.closest('.menu__link')) {
-    Object.keys(obj).forEach((item) => obj[item].classList.remove('active'))
-    body.classList.remove('lock')
+    headerBody.classList.toggle('active')
   }
 })
